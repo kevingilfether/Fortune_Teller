4311044835a2f4de74193de1e4666a7a33652086
kevingilfether <kevingilfether@gmail.com>
2017-09-29 11:02:08 -0400

    Initial Commit

---
+++ Fortune_Teller/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fortune_Teller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //https://www.youtube.com/watch?v=tnCCg3U0tHA
+
+
+        }
+    }
+}
